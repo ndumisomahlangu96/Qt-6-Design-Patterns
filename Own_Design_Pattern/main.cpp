79f8a69cae833824d880bea8d03a73b48a30ebd7
ndumisomahlangu96 <117864978+ndumisomahlangu96@users.noreply.github.com>
2025-12-21 22:39:35 +0200

    Add files via upload

---
+++ Own_Design_Pattern/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * What
+ * Make your own pattern
+ *
+ * Description
+ * Making a pattern is easy-make your own!
+ *
+ * Why
+ * To show a pattern is really just a simpler way ot doing things.
+ *
+ * Example
+ * EasyDevice Pattern
+ * (I totally made that up)
+ */
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
