42d64212fed5b47a6f6d60933477af896512b8c8
ndumisomahlangu96 <117864978+ndumisomahlangu96@users.noreply.github.com>
2026-01-09 21:42:02 +0200

    Add files via upload

---
+++ Open_Closed_Principle/shop.cpp
@@ -0,0 +1,14 @@
+#include "shop.h"
+
+Shop::Shop(QObject *parent)
+    : QObject{parent}
+{}
+
+void Shop::sellDrink(QStringList list, iBrew *machine)
+{
+    qInfo() << "Take the order?";
+    machine->addIngredients(list);
+    machine->brew();
+    qInfo() << "Take payment.";
+    qInfo() << "Give the customer their order.";
+}
---
+++ Open_Closed_Principle/shop.h
@@ -0,0 +1,18 @@
+#ifndef SHOP_H
+#define SHOP_H
+
+#include <QObject>
+#include <QDebug>
+#include "Interface/iBrew.h"
+
+class Shop : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Shop(QObject *parent = nullptr);
+    void sellDrink(QStringList list, iBrew *machine);
+
+signals:
+};
+
+#endif // SHOP_H
