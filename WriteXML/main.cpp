417ff1449a6b3fafc6eb31bca8e953fba5499c6e
ndumisomahlangu96 <117864978+ndumisomahlangu96@users.noreply.github.com>
2026-01-19 05:24:14 +0200

    Add files via upload

--- WriteXML/main.cpp
+++ WriteXML/main.cpp
@@ -6,6 +6,47 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    // Write XML
+    QDomDocument document;
+
+    // Make the root element
+    QDomElement root = document.createElement("Books");
+
+    // Add it to the document
+    document.appendChild(root);
+
+    // Add some elements
+    for(int i = 0; i < 10; i++)
+    {
+        QDomElement node = document.createElement("Books");
+        node.setAttribute("Name","My Book" + QString::number(i));
+        node.setAttribute("ID",QString::number(i));
+        root.appendChild(node);
+
+        for(int i = 0; i < 10; i++)
+        {
+            QDomElement subnode = document.createElement("Chapter");
+            subnode.setAttribute("Name","My Chapter" + QString::number(i));
+            subnode.setAttribute("ID",QString::number(i));
+            root.appendChild(subnode);
+        }
+    }
+
+    // Write to file
+    // CHANGED: Using a relative path saves the file in the application's working directory
+    QFile file("WriteXML.xml");
+    if(!file.open(QIODevice::WriteOnly|QIODevice::Text))
+    {
+        qDebug() << "Failed to open file for writing.";
+        return -1;
+    }
+    else
+    {
+        QTextStream stream(&file);
+        stream << document.toString();
+        file.close();
+        qDebug() << "Finished";
+    }
 
     return a.exec();
 }
