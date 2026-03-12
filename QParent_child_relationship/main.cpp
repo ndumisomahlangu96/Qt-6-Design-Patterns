c37e2c5ca130373959ddc201de3c16a18f9e63dd
Kutlwano Ndumiso Mahlangu <117864978+ndumisomahlangu96@users.noreply.github.com>
2026-03-13 00:16:02 +0200

    Add files via upload

---
+++ QParent_child_relationship/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * Qt memory management
+ * Parent child relationshipl
+ */
+
+#include <QCoreApplication>
+#include <QTimer>
+#include <QDebug>
+#include "test.h"
+
+//Setting in the constructor
+Test* getTest(QObject* parent)
+{
+    return new Test(parent);
+}
+
+//Setting up after constructor
+Test* getTest()
+{
+    return new Test();  // No parent
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QTimer timer;
+    timer.singleShot(3000,&a,&QCoreApplication::quit);
+
+    //Setting in the constructor
+    Test* dog = getTest(&a);
+    dog->setObjectName("Spot");
+
+    //Setting up after constructor
+    Test* cat = getTest();
+    cat->setObjectName("Princess");
+    cat->setParent(dog);
+
+    int value = a.exec();
+    qInfo() << "Exit code: " << value;
+
+
+    return value;
+}
