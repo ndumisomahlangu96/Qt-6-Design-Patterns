3346af787012f8523b04fb5cbb624411913e37fe
Kutlwano Ndumiso Mahlangu <117864978+ndumisomahlangu96@users.noreply.github.com>
2026-04-10 10:19:14 +0200

    Add files via upload

--- QDirModel/dialog.cpp
+++ QDirModel/dialog.cpp
@@ -10,7 +10,15 @@ Dialog::Dialog(QWidget *parent)
     model = new QFileSystemModel(this);
     model->setReadOnly(false);
 
+    // 1. Get the user's home directory path
+    QString userPath = QDir::homePath();
+
+    // 2. Tell the model to start reading the file system at this path
+    model->setRootPath(userPath);
     ui->treeView->setModel(model);
+
+    // 3. Tell the view to focus only on this specific directory
+    ui->treeView->setRootIndex(model->index(userPath));
 }
 
 Dialog::~Dialog()
